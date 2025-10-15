0f2bc00ef51589bb371bcd1eb4172168a94ad22d
Tommy Schnabel-Jones <tommyschnabel@gmail.com>
2025-10-15 11:19:46 -0400

    Added ocr setup

---
+++ ocr/ocr.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+files="$(find ../static -name *.pdf)"
+for file in ${files[@]}; do
+    target="${file%.pdf}_ocr.zip"
+    extracted="${file%.pdf}_OCR.txt"
+    resultPDF="${file%.pdf}_OCR.pdf"
+    echo "Starting $file -> $target"
+
+    if [[ -e $target ]]; then
+        echo "$target already exists, skipping ocr..."
+    else
+        curl -X 'POST' \
+            'http://localhost:8080/api/v1/misc/ocr-pdf' \
+            -H 'accept: */*' \
+            -H 'Content-Type: multipart/form-data' \
+            -F 'removeImagesAfter=true' \
+            -F 'clean=true' \
+            -F 'deskew=true' \
+            -F 'cleanFinal=true' \
+            -F 'ocrRenderType=hocr' \
+            -F "fileInput=@$file;type=application/pdf" \
+            -F 'ocrType=skip-text' \
+            -F 'languages=eng' \
+            -F 'sidecar=true' \
+            --output $target
+        echo "OCR Finished for $file"
+    fi
+
+    echo "Unzipping $target..."
+    unzip -o $target -d "$(dirname $target)"
+    rm $resultPDF # not needed
+
+    echo -e "Finished with $file\n\n"
+done
